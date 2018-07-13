bc3189379aabb69f9b80542d6255e8e289f0b3b8
Anisha <anishamohanty23@gmail.com>
2018-07-13 10:37:11 +0530

    Added

---
+++ python/Node.py
@@ -0,0 +1,8 @@
+class Node(object):
+
+    def __init__(self,character):
+        self.character = character
+        self.leftNode = None
+        self.middleNode = None
+        self.rightNode = None
+        
